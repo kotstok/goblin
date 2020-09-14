e766520aab82530c8e898c5b8b7898911148f3a9
Birca Vadim <vadim.birca@bpay.md>
2020-09-14 23:33:00 +0300

    [new] additional files to the case

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/kotstok/goblin
+
+go 1.13
+
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/websocket v1.4.2
+	github.com/sirupsen/logrus v1.6.0
+)
---
+++ internal/app/ws/ws.go
@@ -0,0 +1,29 @@
+package ws
+
+import (
+	"log"
+	"net/http"
+)
+
+func Start() *Hub {
+	hub := newHub()
+	go hub.run()
+
+	return hub
+}
+
+// serveWs handles websocket requests from the peer.
+func HandleRegister(hub *Hub, w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
+	client.hub.register <- client
+
+	// Allow collection of memory referenced by the caller by doing all work in
+	// new goroutines.
+	go client.writePump()
+	go client.readPump()
+}
